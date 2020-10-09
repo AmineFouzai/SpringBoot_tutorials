1690f70d2cd40eabaebecd37ba6ce86e6eab03ae
MedAmineFouzai <54685665+MedAmineFouzai@users.noreply.github.com>
2020-10-09 21:13:56 +0100

    Delete VehicleRepository.java

--- TP1/src/main/java/com/example/MedAmineFouzai/Repositorys/VehicleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.MedAmineFouzai.Repositorys;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.MedAmineFouzai.Entites.Vehicle;
-
-public interface VehicleRepository extends  JpaRepository<Vehicle,Long>{
-
-}
