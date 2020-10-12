6aaf3d2b89bdd89eb6fab4ee84d67ce99e88e8e1
MedAmineFouzai <54685665+MedAmineFouzai@users.noreply.github.com>
2020-10-12 15:22:17 +0100

    Update VechileController.java

--- TP3/src/main/java/com/example/demo/Controllers/VechileController.java
+++ TP3/src/main/java/com/example/demo/Controllers/VechileController.java
@@ -25,28 +25,28 @@ public class VechileController {
 	
 	
 	@RequestMapping(value="/",
-					method=RequestMethod.GET)
+			method=RequestMethod.GET)
 	public String showCreate()
 	{
 		return "createVehicle";
 	}
 	
 	@RequestMapping(value="/saveVehicle",
-					method=RequestMethod.POST)
+			method=RequestMethod.POST)
 	public String saveVehicle(@ModelAttribute("vehicle") Vehicle vehicle, 
-							  @RequestParam("date") @DateTimeFormat(pattern= "yyyy-MM-dd") Date date,
-							  ModelMap modelMap) throws ParseException 
+				  @RequestParam("date") @DateTimeFormat(pattern= "yyyy-MM-dd") Date date,
+				  ModelMap modelMap) throws ParseException 
 	{
 		
         
 		 vehicle.setVehicleCreationDate(date);
-         Vehicle savedVehicle = vehicleService.addVehicle(vehicle);
+                 Vehicle savedVehicle = vehicleService.addVehicle(vehicle);
 		 modelMap.addAttribute("msg", "vehicle successfully saved  with ID ["+savedVehicle.getVehicleId()+"]");
-		return showCreate();
+		 return showCreate();
 	}
 	
 	@RequestMapping(value="/listeVehicles",
-					method=RequestMethod.GET)
+			method=RequestMethod.GET)
 	public String listeVehicles(ModelMap modelMap)
 	{
 		List<Vehicle> vehicles = vehicleService.getAllVehicles();
@@ -55,7 +55,7 @@ public class VechileController {
 	}
 	
 @RequestMapping(value="/deleteVehicle",
-				method=RequestMethod.GET)
+		method=RequestMethod.GET)
 	public String supprimerArbre(@RequestParam("id") Long id,ModelMap modelMap)
 	{
 		Vehicle vehicle= new Vehicle();
@@ -64,7 +64,8 @@ public class VechileController {
 		return listeVehicles(modelMap);
 	}
 
-	@RequestMapping("/changeVehicle")
+	@RequestMapping(value="/changeVehicle",
+			method=RequestMethod.GET)
 	public String editerArbre(@RequestParam("id") Long id,ModelMap modelMap)
 	{
 		Vehicle vehicle= vehicleService.getVehicle(id);
@@ -73,15 +74,16 @@ public class VechileController {
  	}
 	
 
-	@RequestMapping("/updateVehicle")
+	@RequestMapping(value="/updateVehicle",
+			method=RequestMethod.POST)
 	public String updateArbre(@ModelAttribute("vehicle") Vehicle vehicle,
-							  @RequestParam("date")  @DateTimeFormat(pattern= "yyyy-MM-dd")  Date date,
-			                  ModelMap modelMap) throws ParseException 
+				  @RequestParam("date")  @DateTimeFormat(pattern= "yyyy-MM-dd")  Date date,
+			          ModelMap modelMap) throws ParseException 
 	{
 		
 		 vehicle.setVehicleCreationDate(date);
-         vehicleService.updateVehicle(vehicle);
-         List<Vehicle> vehicles = vehicleService.getAllVehicles();
+         	 vehicleService.updateVehicle(vehicle);
+         	 List<Vehicle> vehicles = vehicleService.getAllVehicles();
 		 modelMap.addAttribute("vehicles", vehicles);	
 		 return listeVehicles(modelMap);
 	}
